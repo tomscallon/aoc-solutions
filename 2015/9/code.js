3e6db9c9f8cc0d8e9993d6d372f2ef19d0bb78e5
Tom Scallon <tssnowguy13@gmail.com>
2017-11-29 15:40:59 -0600

    Complete 2015, day 9.

---
+++ 2015/9/code.js
@@ -0,0 +1,78 @@
+// Tom Scallon. Advent of Code 2015, day 9.
+
+// Read in input.
+const parseLine = line => {
+  const [, p1, p2, d] = /([A-Za-z]+) to ([A-Za-z]+) = (\d+)/.exec(line);
+
+  return {p1, p2, d: +d};
+};
+
+const input = require('fs')
+  .readFileSync(__dirname + '/input.txt', 'utf8')
+  .trim().split('\n')
+  .map(parseLine);
+
+// Part 1 code.
+const toDistMap = (map, {p1, p2, d}) => {
+  p1 in map ?
+    map[p1][p2] = d :
+    map[p1] = {[p2]: d};
+
+  p2 in map ?
+    map[p2][p1] = d :
+    map[p2] = {[p1]: d};
+
+  return map;
+};
+
+const permutations = (arr) => {
+  if (arr.length === 0) {
+    return [];
+  } else if (arr.length === 1) {
+    return [[arr[0]]];
+  }
+
+  let perms = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const tail = arr[i];
+    const headPerms = permutations(arr.slice(0, i).concat(arr.slice(i + 1)));
+
+    perms = perms.concat(headPerms.map(arr => arr.concat(tail)));
+  }
+
+  return perms;
+};
+
+const totalDistance = (perm, distMap) => {
+  let dist = 0;
+
+  for (let i = 1; i < perm.length; i++) {
+    dist += distMap[perm[i - 1]][perm[i]];
+  }
+
+  return dist;
+}
+
+const p1 = () => {
+  const distMap = input.reduce(toDistMap, {});
+  const places = Object.keys(distMap);
+
+  return permutations(places)
+    .map(p => totalDistance(p, distMap))
+    .sort((a, b) => a - b)[0];
+};
+
+// Part 2 code.
+const p2 = () => {
+  const distMap = input.reduce(toDistMap, {});
+  const places = Object.keys(distMap);
+
+  return permutations(places)
+    .map(p => totalDistance(p, distMap))
+    .sort((a, b) => b - a)[0];
+};
+
+// Export the functions.
+exports[1] = p1;
+exports[2] = p2;

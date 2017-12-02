d3757635bc9cd21ca7e22b987a5af603f0cb7caf
Tom Scallon <tssnowguy13@gmail.com>
2017-12-01 20:35:55 -0600

    Complete 2015, day 17.

---
+++ 2015/17/code.js
@@ -0,0 +1,60 @@
+// Tom Scallon. Advent of Code 2015, day 17.
+const utils = require('../../utils');
+
+// Read in input.
+const input = require('fs')
+  .readFileSync(__dirname + '/input.txt', 'utf8')
+  .trim().split('\n')
+  .map(x => +x);
+
+const capacity = 150;
+
+// Part 1 code.
+const p1 = () => {
+  const sums = utils.summationsUsing(
+    input,
+    capacity,
+    {
+      repeat: false,
+      unique: true
+    }
+  );
+
+  let count = 0;
+
+  for (let sum of sums) {
+    count++;
+  }
+
+  return count;
+};
+
+// Part 2 code.
+const p2 = () => {
+  const sums = utils.summationsUsing(
+    input,
+    capacity,
+    {
+      repeat: false,
+      unique: true
+    }
+  );
+
+  let min = input.length + 1;
+  let count = 0;
+
+  for (let sum of sums) {
+    if (sum.length < min) {
+      min = sum.length;
+      count = 1;
+    } else if (sum.length === min) {
+      count++;
+    }
+  }
+
+  return [min, count];
+};
+
+// Export the functions.
+exports[1] = p1;
+exports[2] = p2;
--- utils.js
+++ utils.js
@@ -1,3 +1,5 @@
+const repeat = (x, n) => n === 0 ? [] : [x].concat(repeat(x, n - 1));
+
 const permutations = (arr) => {
   if (arr.length === 0) {
     return [];
@@ -73,5 +75,93 @@ function* summations(n, x, {unique = false, min = 0, max = x} = {}) {
   }
 };
 
+/**
+ * Enumerates all summations to `x` involving only the numbers in `a`. For
+ * example, summationsUsing([4, 3], 16) would yield [3, 3, 3, 3, 4] and
+ * [4, 4, 4, 4], along with permutations of those options. We assume `x`, along
+ * with every element of `a` to be positive.
+ *
+ * This function is a generator, and should be iterated using `for..of`.
+ *
+ * The options available are:
+ *   `unique` -- if set to `true`, then permutations of options will not be
+ *     returned. (Combinations are yielded rather than permutations.) Defaults
+ *     to `false`.
+ *   `repeat` -- if set to `true`, then numbers provided in `a` cannot be
+ *     reused. For example, summationsUsing([4, 3], 16, {repeat = false}) would
+ *     yield nothing, as 3 + 4 is 7 (not 16). When `repeat` is `true`, the
+ *     elements of `a` are assumed to be unique. Defaults to `true`.
+ *   `sorted` -- helper flag used when `unique` is `true`. You can also supply
+ *     `true` here to indicate `a` is already sorted DESCENDING. Defaults to
+ *     `false`.
+ */
+function* summationsUsing(a, x, {unique = false, repeat: _repeat = true, sorted = false} = {}) {
+  if (x === 0) {
+    // We met the required sum. Yield an empty array and exit.
+    yield [];
+    return;
+  } else if (a.length === 0) {
+    // If we have no options to add with, there
+    // are no possible results.
+
+    return;
+  } else if (a.length === 1) {
+    // If we have only one option, then the only way we have a
+    // solution is if x is divisible by that option.
+    if (_repeat && x % a[0] === 0 || x === a[0]) {
+      yield repeat(a[0], x / a[0]);
+    }
+
+    return;
+  }
+
+  // Otherwise, we need to do this recursively.
+  if (!sorted && unique) {
+    // As in `summations`, if uniqueness is requested, we'll do
+    // this by making any solution appear in descending order.
+    // To achieve this, we must first sort the list of options.
+    a = a.slice(0).sort((a, b) => b - a);
+  }
+
+  // For each option...
+  for (let i = 0; i < a.length; i++) {
+    const e = a[i];
+
+    if (e > x) {
+      // This option is too large. Skip it.
+      continue;
+    }
+
+    const restSums = summationsUsing(
+      unique ?
+        // Enforcing unique results. This is done by only yielding solutions
+        // whose elements are ordered descendingly. Thus, we slice the array
+        // to only return elements to the right of the current one (since we
+        // have sorted `a`). If we're not allowing repeats, we also exclude
+        // the current element (hence the + 1 if `repeat` = `false`).
+        a.slice(i + (_repeat ? 0 : 1)) :
+
+        // Not enforcing uniqueness.
+        _repeat ?
+          // If we allow repeats, then we just submit the array of options as is.
+          a :
+          // If not, remove the used element before submitting the options.
+          a.slice(0, i).concat(a.slice(i + 1)),
+      x - e,
+
+      // Preserve all options, except set `sorted` to `true`
+      // to accelerate subsequent calls.
+      {unique, repeat: _repeat, sorted: true}
+    );
+
+    // For each determined summation of x - e...
+    for (let sum of restSums) {
+      yield [e, ...sum];
+    }
+  }
+}
+
+exports.repeat = repeat;
 exports.permutations = permutations;
 exports.summations = summations;
+exports.summationsUsing = summationsUsing;

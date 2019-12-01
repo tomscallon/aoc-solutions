d1aeac6f89719be3a7bad058c82ae8c7d47e740b
Tom Scallon <tssnowguy13@gmail.com>
2019-11-30 21:41:59 -0800

    Solve js/2019/1

--- solution/js/2019/1.js
+++ solution/js/2019/1.js
@@ -2,16 +2,30 @@
 
 // Read in input.
 const {input} = process.env;
-const lines = input.split('\\n');
+const lines = input.trim().split('\n');
 
 // Part 1 code.
 const p1 = () => {
-  throw 'Not yet implemented';
+  return lines
+    .map(l => Math.floor(+l/3) - 2)
+    .reduce((s, l) => s + l, 0);
 };
 
 // Part 2 code.
+const fuel2 = x => {
+  if (x > 0) {
+    let xp = Math.max(0, Math.floor(x / 3) - 2);
+    return xp + fuel2(xp);
+  } else {
+    return 0;
+  }
+}
+
 const p2 = () => {
-  throw 'Not yet implemented';
+  return lines
+    .map(Number)
+    .map(fuel2)
+    .reduce((x, y) => x + y, 0);
 };
 
 // Export the functions.

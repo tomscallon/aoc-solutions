66da96cf1b8f96d9b3e72b4731427de90dd2236c
Tom Scallon <tssnowguy13@gmail.com>
2019-12-02 21:45:10 -0800

    Implement a big boy solution for js/2019/3

--- solution/js/2019/3.js
+++ solution/js/2019/3.js
@@ -8,75 +8,73 @@ const wires = lines.map(l => l.split(',').map(s => ({
   a: +s.substring(1),
 })));
 
-const getAllPositions = wire => {
-  let pos = {x: 0, y: 0};
-  const all = ['0,0'];
-
-  wire.forEach(({d, a}) => {
-    const diff = {
-      x: d === 'R' ? 1 : d === 'L' ? -1 : 0,
-      y: d === 'U' ? 1 : d === 'D' ? -1 : 0,
-    };
-
-    for (let i = 0; i < a; i++) {
-      pos = {
-        x: pos.x + diff.x,
-        y: pos.y + diff.y,
-      };
-      all.push(`${pos.x},${pos.y}`);
-    }
-  });
-
-  return new Set(all);
+const DIFFS = {
+  R: {x:  1, y:  0},
+  L: {x: -1, y:  0},
+  U: {x:  0, y:  1},
+  D: {x:  0, y: -1},
 };
 
-const intersect = (a, b) => a.filter(x => b.includes(x));
-
-// Part 1 code.
-const p1 = () => {
-  const [all1, all2] = wires.map(getAllPositions);
-  const intersections = intersect(all1, all2)
-    .map(s => s.split(',').map(Number).reduce((x, y) => Math.abs(x) + Math.abs(y), 0))
-    .filter(x => x !== 0);
-  return Math.min(...intersections);
-};
-
-const getAllPositions2 = wire => {
+const iteratePositions = (wire, fn) => {
   let pos = {x: 0, y: 0};
-  const all = {};
   let dist = 0;
 
   wire.forEach(({d, a}) => {
-    const diff = {
-      x: d === 'R' ? 1 : d === 'L' ? -1 : 0,
-      y: d === 'U' ? 1 : d === 'D' ? -1 : 0,
-    };
+    const diff = DIFFS[d];
 
     for (let i = 0; i < a; i++) {
-      pos = {
+      fn(pos = {
         x: pos.x + diff.x,
         y: pos.y + diff.y,
-      };
-      const key = `${pos.x},${pos.y}`;
-      ++dist;
-      if (!(key in all)) {
-        all[key] = dist;
+      }, ++dist);
+    }
+  });
+};
+
+// Part 1 code.
+const p1 = () => {
+  const all = {};
+
+  iteratePositions(wires[0], ({x, y}) => all[`${x},${y}`] = true);
+  delete all['0,0'];
+
+  let closest = null;
+  iteratePositions(wires[1], ({x, y}) => {
+    const key = `${x},${y}`;
+    if (key in all) {
+      const dist = Math.abs(x) + Math.abs(y);
+      if (closest == null || dist < closest) {
+        closest = dist;
       }
     }
   });
 
-  return all;
+  return closest;
 };
 
 // Part 2 code.
 const p2 = () => {
-  const [all1, all2] = wires.map(getAllPositions2);
-  const [pos1, pos2] = [all1, all2].map(Object.keys);
-  const intersections = intersect(pos1, pos2)
-    .map(x => all1[x] + all2[x])
-    .filter(x => x !== 0);
+  const all = {};
 
-  return Math.min(...intersections);
+  iteratePositions(wires[0], ({x, y}, dist) => {
+    const key = `${x},${y}`;
+    if (!(key in all)) {
+      all[key] = dist;
+    }
+  });
+  delete all['0,0'];
+
+  let closest = null;
+  iteratePositions(wires[1], ({x, y}, dist) => {
+    const key = `${x},${y}`;
+    if (key in all) {
+      const totalDist = all[key] + dist;
+      if (closest == null || totalDist < closest) {
+        closest = totalDist;
+      }
+    }
+  });
+  return closest;
 };
 
 // Export the functions.
